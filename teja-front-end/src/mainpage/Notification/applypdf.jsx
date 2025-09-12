31389de9d0164158915451098a74f62e1708f056
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-09-12 20:15:06 +0530

    Update applypdf.jsx

--- teja-front-end/src/mainpage/Notification/applypdf.jsx
+++ teja-front-end/src/mainpage/Notification/applypdf.jsx
@@ -3,7 +3,7 @@ import './applypdf.css'
 import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 import html2pdf from 'html2pdf.js';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const Applicationpdf = () => {
   const { registrationNumber } = useParams();
